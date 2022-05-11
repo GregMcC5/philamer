970ba0c459f266474e487932e9f934478da5389e
GregMcC5 <gregmcc@umich.edu>
2022-05-11 13:39:36 -0400

    Update ocr_test.py

--- dif/best_values/ocr_test.py
+++ dif/best_values/ocr_test.py
@@ -19,7 +19,7 @@ img = np.array(Image.open(filename))
 norm_img = np.zeros((img.shape[0], img.shape[1]))
 img = cv2.normalize(img, norm_img, 0, 255, cv2.NORM_MINMAX)
 img = cv2.threshold(img, 100, 255, cv2.THRESH_BINARY)[1]
-#img = cv2.GaussianBlur(img, (1, 1), 0)
+img = cv2.GaussianBlur(img, (1, 1), 0)
 
 cv2.imwrite("normalized_img.png", img)
 
